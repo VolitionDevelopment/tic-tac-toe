5e27f00448b768f878b624dbdb7ffc47e7ef2fb2
Jackson Yeager <jacksonyeager@gmail.com>
2016-07-22 09:29:13 -0400

    AI Update
    AI is now learning

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var turn = 0;
 var winner = false;
+var players = 1;
+var moves = [];
 var winningSolutions = [
     [1, 2, 3],
     [4, 5, 6],
@@ -11,22 +13,135 @@ var winningSolutions = [
     [3, 5, 7]
 ];
 
+function sendData(board){
+    var data = new FormData();
+    data.append("data" , board);
+    var xhr = new XMLHttpRequest();
+    xhr.open( 'post', 'request.php', true);
+    console.log("Sent data to server: " + board);
+    xhr.send(data);
+}
+
+function setPlayers(num){
+    players = num;
+
+    $('.player-buttons').addClass('hidden');
+    $('.game-wrapper').removeClass('hidden');
+}
+
 function markSquare(square){
     if(!winner){
         if(square.innerHTML === "-") {
             if (turn % 2 == 0) {
                 square.innerHTML = "X";
+                moves.push(square.id);
                 update();
-                ai();
+                if(players === 1){
+                    aiLearning();
+                }
+            }else{
+                square.innerHTML = "O";
+                update();
             }
         }
     }
 }
 
-function ai(){
+function choosePossibleMove(not){
+    var possibleMove = Math.floor(Math.random() * 9) + 1;
+
+    if(element(possibleMove).innerHTML !== "-"){
+        choosePossibleMove();
+    }
+
+    if(not != undefined){
+        if(possibleMove === not){
+            choosePossibleMove(not);
+        }
+    }
+
+    return possibleMove;
+}
+
+function getTxt(){
+    var response = $.ajax({
+        url:'games.txt',
+        async: false
+    });
+
+    var lines = response.responseText.split("\n");
+    var games = [];
+
+    for (var i = 0; i < lines.length; i++) {
+        var obj = lines[i];
+
+        games.push([obj.split(" ")[0], obj.split(" ")[1]]);
+    }
+
+    console.log(games);
+
+    return games;
+}
+
+function aiLearning(){
+    var games = getTxt();
+    var winningPaths = [];
+    var tiePaths = [];
+    var lossPaths = [];
+
+    for (var i = 0; i < games.length; i++) {
+        var game = games[i];
+
+        for (var j = 0; j < game.length; j++) {
+            var move = game[0].split(",");
+            var result = game[1];
+
+            for (var k = 0; k < move.length; k++) {
+                var knownMove = move[k];
+
+
+                if(moves[turn - 1] === knownMove){
+                    if(result === "WON"){
+                        winningPaths.push(game);
+                    }
+
+                    if(result === "TIE"){
+                        tiePaths.push(game);
+                    }
+
+                    if(result === "LOSS"){
+                        lossPaths.push(game);
+                    }
+                }
+            }
+        }
+    }
+
+    if(winningPaths.length > 0){
+        var randMove = Math.floor(Math.random() * winningPaths.length) + 1;
+        element(randMove).innerHTML = "O";
+        moves.push(randMove);
+        update();
+        console.log("Winning move found. Selecting random winning move.");
+        return;
+    }else if(tiePaths.length > 0){
+        var randMove = Math.floor(Math.random() * tiePaths.length) + 1;
+        element(randMove).innerHTML = "O";
+        moves.push(randMove);
+        update();
+        console.log("Tie move found. Selecting random winning move.");
+        return;
+    }
+
+    console.log("No successful move found. Falling back to AI heuristics");
+    aiHeuristics();
+}
+
+function aiHeuristics(not){
     if(!winner){
         if(element(5).innerHTML === "-"){
             element(5).innerHTML = "O";
+            moves.push(5);
             update();
         }else{
             for(var i = 0; i < winningSolutions.length; i++){
@@ -40,6 +155,7 @@ function ai(){
                     }
 
                     element(winningSolutions[i][2]).innerHTML = "O";
+                    moves.push(winningSolutions[i][2]);
                     update();
                     return;
                 }
@@ -54,6 +170,7 @@ function ai(){
                     }
 
                     element(winningSolutions[i][0]).innerHTML = "O";
+                    moves.push(winningSolutions[i][0]);
                     update();
                     return;
                 }
@@ -68,6 +185,7 @@ function ai(){
                     }
 
                     element(winningSolutions[i][1]).innerHTML = "O";
+                    moves.push(winningSolutions[i][1]);
                     update();
                     return;
                 }
@@ -79,6 +197,7 @@ function ai(){
 
                 if(element(obj).innerHTML === "-"){
                     element(obj).innerHTML = "O";
+                    moves.push(obj);
                     update();
                     return;
                 }
@@ -88,9 +207,10 @@ function ai(){
 
             if(element(square).innerHTML === "-"){
                 element(square).innerHTML = "O";
+                moves.push(square);
                 update();
             }else{
-                ai();
+                aiHeuristics();
             }
         }
     }
@@ -107,8 +227,10 @@ function update(){
 
             if(turn % 2 === 0){
                 element("turn").innerHTML = "WINNER: X";
+                sendData(moves.join() + " LOSS");
             }else{
                 element("turn").innerHTML = "WINNER: 0";
+                sendData(moves.join() + " WON");
             }
 
             for (var j = 0; j < winningSolutions[i].length; j++) {
@@ -117,6 +239,7 @@ function update(){
             }
 
             winner = true;
+
         }
     }
 
@@ -124,6 +247,7 @@ function update(){
 
     if(turn === 9 && !winner){
         element("turn").innerHTML = "END GAME";
+        sendData(moves.join() + " TIE");
     }
 }
 
